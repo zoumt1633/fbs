b2d862394501d1ab7c0b76dc072cb0c0a02a6daa
zoumt1633 <blueskyandtai@163.com>
2023-03-21 10:17:50 +0800

    去除低版本python3限制

--- fbs/__init__.py
+++ fbs/__init__.py
@@ -14,19 +14,21 @@ SETTINGS['app_name'], which you define in src/build/settings/base.json.
 """
 SETTINGS = _state.SETTINGS
 
+
 def init(project_dir):
     """
     Call this if you are invoking neither `fbs` on the command line nor
     fbs.cmdline.main() from Python.
     """
-    if sys.version_info[0] != 3 or sys.version_info[1] not in (5, 6):
+    if sys.version_info[0] != 3:
         raise FbsError(
-            'The free version of fbs only supports Python 3.5 and 3.6.\n'
+            'The free version of fbs only supports Python 3\n'
             'Please obtain fbs Pro from https://build-system.fman.io/pro.'
         )
     SETTINGS.update(get_core_settings(abspath(project_dir)))
     for profile in get_default_profiles():
         activate_profile(profile)
+
 
 def activate_profile(profile_name):
     """
